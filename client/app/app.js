acfd9376755b2be7ccdbb22d6658f23d13919a16
Carlos Rios <hello@carlosrios.co.uk>
2016-04-29 13:47:53 +0100

    lets go

--- client/app/app.js
+++ client/app/app.js
@@ -3,7 +3,7 @@ import uiRouter from 'angular-ui-router';
 import Common from './common/common';
 import Components from './components/components';
 import AppComponent from './app.component';
-import 'normalize.css';
+import 'purecss';
 
 angular.module('app', [
     uiRouter,
@@ -12,8 +12,6 @@ angular.module('app', [
   ])
   .config(($locationProvider) => {
     "ngInject";
-    // @see: https://github.com/angular-ui/ui-router/wiki/Frequently-Asked-Questions
-    // #how-to-configure-your-server-to-work-with-html5mode
     $locationProvider.html5Mode(true).hashPrefix('!');
   })
 
